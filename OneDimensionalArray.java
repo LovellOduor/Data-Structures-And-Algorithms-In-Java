4153d97819b846797cfa357e1dad69f64aca57a6
unknown <lovelloduor123@gmail.com>
2022-09-21 12:03:13 +0300

    OneDimensionalArray findmax, min, secondmax and reverse

--- OneDimensionalArray.java
+++ OneDimensionalArray.java
@@ -7,6 +7,7 @@ public class OneDimensionalArray{
             }
             System.out.println();
         }
+
         // Remove even integers from an array
         int[] removeEven(int arr[]){
             int n = arr.length;
@@ -27,6 +28,56 @@ public class OneDimensionalArray{
             return oddArray;
         }
 
+        // Reverse an array in Java
+        public int[] reversArray(int[] numbers,int start, int end){
+            int temp;
+            while(start < end){
+                temp = numbers[start];
+                numbers[start] = numbers[end];
+                numbers[end] = temp;
+                start++;
+                end--;
+            }
+            return numbers;
+        }
+
+        // find minimum value in an array
+        public int findMin(int arr[]){
+            int minVal = arr[0];
+            for(int i=0;i<arr.length;i++){
+                if(arr[i]<minVal){
+                    minVal = arr[i];
+                }
+            }
+            return minVal;
+        }
+
+        // find maximum value in an array
+        public int findMax(int[] arr){
+            int maxVal = arr[0];
+            for(int i=0;i<arr.length;i++){
+                if(arr[i]>maxVal){
+                    maxVal = arr[i];
+                }
+            }
+            return maxVal;
+        }
+
+       // Find the second maximum value in an array
+       public int findSecondMax(int arr[]){
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] > max){
+                secondMax = max;
+                max = arr[i];
+            }else if(arr[i]>secondMax && arr[i] != max){
+                secondMax = arr[i];
+            }
+        }
+        return secondMax;
+       }
+
         public void arrayDemo(){
             int[] myArray = new int[5];
             printArray(myArray);
@@ -35,9 +86,12 @@ public class OneDimensionalArray{
 
     public static void main(String[] args){
         OneDimensionalArray arrutil = new OneDimensionalArray();
-        arrutil.arrayDemo();
-        int[] nums = {1,2,3,4,5,6,7,8,9};
-        arrutil.printArray(arrutil.removeEven(nums));
+        //arrutil.arrayDemo();
+        int[] nums = {1,2,3,4,20,5,6,7,8,9};
+        //arrutil.printArray(arrutil.removeEven(nums));
+        //arrutil.printArray(arrutil.reversArray(nums,0,nums.length-1));
+        System.out.println(arrutil.findSecondMax(nums));
+
     }
 
 }
