24ca40ad22fdf57c32e22029d855f5a12b8b21fb
unknown <lovelloduor123@gmail.com>
2023-01-17 14:28:16 +0300

    Completed circular singly linked list notes

--- CircularSinglyLinkedList.java
+++ CircularSinglyLinkedList.java
@@ -1,3 +1,10 @@
+// Circular Singly Linked List
+/*
+It's similar to a Singly Linked List. The only difference is that the last node points to the
+first node. 
+However, rather than tracking the head node, we keep track of the last/tail node.
+*/
+
 public class CircularSinglyLinkedList{
     private ListNode last;
     private int length; 
@@ -22,6 +29,10 @@ public class CircularSinglyLinkedList{
     } 
 
     // Create a Circular Singly Linked List
+    /* 
+    Similar to a SinglyLinked List but the last node's next value should point to the
+    first node.
+    */
     public void createCircularSinglyLinkedList(){
         ListNode first = new ListNode(1);
         ListNode second = new ListNode(2);
@@ -37,6 +48,11 @@ public class CircularSinglyLinkedList{
         last = fifth;
     }
     // Traverse and display elements of a circular singly linked list
+    /*
+    To access the first node of the list, create a variable that points to the next of the last node.
+    Then in a loop check whether the first node is not equal to the last and print the current node's data.
+    Finally when the loop exits, print the last node that should be stored in the variable referencing the current node.
+    */
     public void display(){
         if(last == null){
             return;
@@ -50,6 +66,12 @@ public class CircularSinglyLinkedList{
     }
 
     // Insert a node at the start of a Circular Singly Linked List
+    /*
+    We consider two scenarios. The first is if the list is empty. 
+    If so then we set the last node to point to the new node.
+    If noe then we set the next node of the new node to point to the next node of the last node.
+    Finally, we can set the last node's next to point to the new node.
+    */
     public void insertFirst(int data){
         ListNode newNode = new ListNode(data);
         if(last == null){
@@ -61,7 +83,14 @@ public class CircularSinglyLinkedList{
        length++;
     }
     
-    //Insert node at the end of a singly linked list
+    //Insert node at the end of a circular singly linked list
+    /*
+    We consider if the list is empty.
+    If so we set the last node to point to the new node,
+    and then the next of the last node to the new node.
+    Otherwise if the list is not empty, we set the next of the new node to the next of the last node.
+    we set the next of the last node to the new node. Then finally we set the last node to the new node.
+    */
     public void insertLast(int data){
         ListNode newNode = new ListNode(data);
         if(last == null){
@@ -74,7 +103,14 @@ public class CircularSinglyLinkedList{
         }
         length++; 
     }
+
     // Remove first node from a Circular Singly Linked List
+    /*
+    We consider if the list has only one node. If so then we simply set the last node to null.
+    If there is more than one item, then we set the next of the last node to the next of the first node.
+    Finally we set the next of the temporary node holding the removed first node to null and return it
+    
+    */
     public ListNode removeFirst(){
         ListNode first = last.next;
         if(first == last){
@@ -87,8 +123,7 @@ public class CircularSinglyLinkedList{
     }
     
     public static void main(String[] args){
-        CircularSinglyLinkedList 
-        csll = new CircularSinglyLinkedList(); 
+        CircularSinglyLinkedList csll = new CircularSinglyLinkedList(); 
         csll.createCircularSinglyLinkedList();
         csll.display();
         csll.insertLast(7);
