606f18c0b6e54bd6e321446848fd4ea5c1cdadba
unknown <lovelloduor123@gmail.com>
2023-01-23 12:44:54 +0300

    Valid parenthesis problem stack

---
+++ StackValidParentheses.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+public class StackValidParentheses{
+    public static boolean isValid(String s){
+        Stack<Character> stack = new Stack<>();
+        for(char c:s.toCharArray()){
+            if(c == '{' || c == '[' || c == '('){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }else{
+                    char x = stack.peek();
+                    if((x == '{' && c =='}') || (x =='(' && c ==')') || (x =='[' && c ==']')){
+                        stack.pop();
+                    }else{
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args){
+        System.out.print(isValid("{[]}"));
+    }
+}
